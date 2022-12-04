c189540a06dde5d6591830c1fea05b242d4f5ae1
alves-patrick <patrickalvesarts@gmail.com>
2022-12-04 20:30:22 -0300

    Initial Commit

---
+++ HabitosPro/HabitosProApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitosProApp.swift
+//  HabitosPro
+//
+//  Created by Patrick Alves on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitosProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
