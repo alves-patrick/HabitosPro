fab9a4eb1be6da217e28ab2bbe8f97a79173979f
alves-patrick <patrickalvesarts@gmail.com>
2022-12-11 19:57:26 -0300

    SplashView

---
+++ HabitosPro/Splash/View/SplashView.swift
@@ -0,0 +1,24 @@
+//
+//  SplashView.swift
+//  HabitosPro
+//
+//  Created by Patrick Alves on 11/12/22.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    
+    var body: some View {
+        Text("Ola")
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+        
+    }
+}
+
+
