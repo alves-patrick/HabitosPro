b73c281d63358a7dc6a48a68836a024b436af1c9
alves-patrick <patrickalvesarts@gmail.com>
2023-01-10 22:28:14 -0300

    tela cadastro 2

--- HabitosPro/SignIn/View/SignInView.swift
+++ HabitosPro/SignIn/View/SignInView.swift
@@ -14,31 +14,54 @@ struct SignInView: View {
     @State var email = ""
     @State var password = ""
     @State var action: Int? = 0
+    @State var navigationHidden = true
     
     
     var body: some View {
-        VStack(alignment: .center) {
-            Image("logo")
-                .resizable()
-                .scaledToFit()
-                .padding(.horizontal, 48)
+        NavigationView {
             
-            Text("Login")
-                .foregroundColor(.orange)
-                .font(Font.system(.title).bold())
-                .padding(.bottom, 8)
-            
-            numberField
-            
-            passwordField
-        
-            enterButton
-            
-            registerLink
-        }
-            
+            ScrollView(showsIndicators: false) {
+                
+                VStack(alignment: .center, spacing: 20) {
+                    
+                    Spacer(minLength: 36)
+                    
+                    VStack(alignment: .center, spacing: 8) {
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .padding(.horizontal, 48)
+                        
+                        Text("Login")
+                            .foregroundColor(.orange)
+                            .font(Font.system(.title).bold())
+                            .padding(.bottom, 8)
+                        
+                        numberField
+                        
+                        passwordField
+                        
+                        enterButton
+                        
+                        registerLink
+                        
+                        Text("Copyright @Patrick")
+                            .foregroundColor(Color.gray)
+                            .font(Font.system(size: 16).bold())
+                            .padding(.top, 16)
+                    }
+                    
+                }
+            }
+                .frame(maxWidth:.infinity, maxHeight: .infinity)
+                .padding(.horizontal, 32)
+                .background(Color.white)
+                .navigationBarTitle("Login", displayMode: .inline)
+                .navigationBarHidden(navigationHidden)
         }
     }
+}
+
     
     extension SignInView {
         var numberField: some View {
