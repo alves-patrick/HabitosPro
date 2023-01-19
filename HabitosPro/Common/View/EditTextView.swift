a6818599b20aa0fb9344bd31f1e6387da1d0797c
Patrick Alves <patrickalvesarts@gmail.com>
2023-01-19 12:56:08 -0300

    Elegante

---
+++ HabitosPro/Common/View/EditTextView.swift
@@ -0,0 +1,19 @@
+//
+//  EditTextView.swift
+//  HabitosPro
+//
+//  Created by Patrick Alves on 19/01/23.
+//
+
+import SwiftUI
+
+struct EditTextView: View {
+    var body: some View {
+        Text("Ola Mundo")
+    }
+}
+struct EditTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditTextView()
+    }
+}
