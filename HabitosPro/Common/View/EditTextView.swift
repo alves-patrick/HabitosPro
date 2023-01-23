e07b667b5ec34e89728f07e5ebd98122d226194d
Patrick Alves <patrickalvesarts@gmail.com>
2023-01-23 09:01:20 -0300

    Binding

--- HabitosPro/Common/View/EditTextView.swift
+++ HabitosPro/Common/View/EditTextView.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 struct EditTextView: View {
+    
+    @Binding var text: String
+    
+    var placeholder: String = ""
+    
+    
+    
     var body: some View {
-        Text("Ola Mundo")
+        TextField(placeholder, text: $text)
     }
 }
 struct EditTextView_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(ColorScheme.allCases, id: \.self) {
             VStack {
-                EditTextView()
+                EditTextView(text: .constant("Texto"),
+            placeholder: "e-mail")
                     .padding()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
