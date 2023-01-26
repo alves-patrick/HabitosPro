d7dcd7ab33e131e9fc00bae88176c3e3690d763a
Patrick Alves <patrickalvesarts@gmail.com>
2023-01-26 18:32:10 -0300

    Correção

--- HabitosPro/SignUp/Model/SignUpRequest.swift
+++ HabitosPro/SignUp/Model/SignUpRequest.swift
@@ -8,23 +8,23 @@
 import Foundation
 
 struct SignUpRequest: Encodable {
-    
-    let fullName: String
-    let email: String
-    let document: String
-    let phone: String
-    let birthday: String
-    let password: String
-    let gender: Int
-    
-    enum CodingKeys: String, CodingKey {
-        case fullname = "name"
-        case email
-        case password
-        case document
-        case phone
-        case birthday
-        case gender
-    }
-    
+  
+  let fullName: String
+  let email: String
+  let password: String
+  let document: String
+  let phone: String
+  let birthday: String
+  let gender: Int
+  
+  enum CodingKeys: String, CodingKey {
+    case fullName = "name"
+    case email
+    case password
+    case document
+    case phone
+    case birthday
+    case gender
+  }
 }
+
