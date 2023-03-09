63b55b6a5e4bd2fa25209af9349e419d80c31073
Patrick <patrickalvesarts@gmail.com>
2023-03-08 22:27:59 -0300

    fix

--- HabitosPro/SignUp/ViewModel/SignUpViewModel.swift
+++ HabitosPro/SignUp/ViewModel/SignUpViewModel.swift
@@ -55,27 +55,27 @@ class SignUpViewModel: ObservableObject {
             if let sucess = sucessResponse {
                 
      //         WebService.login(request: SignInRequest(email: self.email,
-          //                                              password: self.password)) { (successResponse, errorResponse) in
+          //    /                                          password: self.password)) { (successResponse, errorResponse) in
                   
          //        if let errorSignIn = errorResponse {
-          //          DispatchQueue.main.async {
-          //            // Main Thread
-         //             self.uiState = .error(errorSignIn.detail.message)
-         //           }
-         //      }
+          //       DispatchQueue.main.async {
+          //          // Main Thread
+         //            self.uiState = .error(errorSignIn.detail.message)
+         //         }
+         //    }
                   
-        //       if let successSignIn = successResponse {
-        //            DispatchQueue.main.async {
-        //          print(successSignIn)
-        //             self.publisher.send(sucess)
+        //      if let successSignIn = successResponse {
+        //          DispatchQueue.main.async {
+        //         print(successSignIn)
+        //         self.publisher.send(sucess)
        //                 self.uiState = .success
-         //        }
-       //        }
+         //      }
+       //      }
        //
         //   }
-       //     DispatchQueue.main.async {
+       //    DispatchQueue.main.async {
        //
-       //           }
+       //          }
                 }
             }
         }
