5a002bfe404b1030bba7fa162f953f16cf4ed692
Patrick <patrickalvesarts@gmail.com>
2023-03-22 19:56:23 -0300

    fix combine

---
+++ HabitosPro/SignUp/Interactor/SignUpInteractor.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpInteractor.swift
+//  HabitosPro
+//
+//  Created by Patrick Alves on 22/03/23.
+//
+
+import Foundation
+import Combine
+ 
+class SignUpInteractor {
+    
+    private let remote: SignUpRemoteDataSource = .shared
+    // private let local: LocalDataSource
+    
+}
+
+extension SignUpInteractor {
+    
+    func postUser(signUpRequest request: SignUpRequest) -> Future<Bool, AppError> {
+        return remote.login(request: request)
+}
+}
